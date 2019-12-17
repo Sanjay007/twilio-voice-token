62a554c95bcb726f184608b7064f330af2a245ba
Sanjay Yadav <syadav2@yodlee.com>
2019-12-17 14:56:27 +0530

    df

---
+++ index2.js
@@ -0,0 +1,38 @@
+const http = require('http');
+const express = require('express');
+const ClientCapability = require('twilio').jwt.ClientCapability;
+
+const app = express();
+
+app.get('/token', (req, res) => {
+  // put your Twilio API credentials here
+  
+  const accountSid = 'SK8131066812d0822b0ac088184fa571af';
+  const authToken = 'lUSW5oXsLRRaWpnqm0ALdPqkSUa9Qlr0';
+
+  // put your Twilio Application Sid here
+  const appSid = 'AC07d7e7571e6969bc8680d5c2022b443c';
+
+  const capability = new ClientCapability({
+    accountSid: accountSid,
+    authToken: authToken,
+  });
+  capability.addScope(
+    new ClientCapability.OutgoingClientScope({ applicationSid: appSid })
+  );
+  capability.addScope(new ClientCapability.IncomingClientScope('joey'));
+  const token = capability.toJwt();
+
+  res.set('Content-Type', 'application/jwt');
+  res.send(token);
+});
+
+app.post('/voice', (req, res) => {
+  // TODO: Create TwiML response
+});
+
+http.createServer(app).listen(3001, err => {
+    if (err) throw err;
+    console.log('> Ready on http://localhost:3001');
+  });
+console.log('Twilio Client app server running at http://127.0.0.1:1337/token/');
